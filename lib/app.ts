1d5f6ca00826d1296250bf721c2cdb8de7d86034
AleAlves <aleson.a.s@gmail.com>
2020-02-29 18:16:47 -0300

    Data base stuff

--- lib/app.ts
+++ lib/app.ts
@@ -11,7 +11,7 @@ class App {
 
   public app: express.Application;
   public router: Router = new Router();
-  public mongoUrl: string = 'mongodb://127.0.0.1:27017/' + packageInfo.name + '-database';
+  public mongoUrl: string = process.env.DEVELOPMENT_ENV || 'mongodb://127.0.0.1:27017/' + packageInfo.name + '-database';
 
   constructor() {
     this.app = express();
